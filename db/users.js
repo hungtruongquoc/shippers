b34ebe8cc56e4872adbcd28d9cd6dab2a556dfa9
Hung Truong <hungtruongquoc@gmail.com>
2017-03-02 20:43:05 -0600

    Added user model

--- db/users.js
+++ db/users.js
@@ -1,3 +1,36 @@
 /**
  * Created by hungtruong on 3/2/17.
  */
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+let User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+module.exports = {User};
